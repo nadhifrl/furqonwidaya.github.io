dcb5e6d5f17234711d0f65c8a4074da51c38055a
Dimas-Pratama-Purwadinata <54875915+Dimas-Pratama-Purwadinata@users.noreply.github.com>
2019-10-16 20:58:49 +0700

    Update script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,3 +4,36 @@ function back() {
 function forward() {
   alert("Ini adalah Halaman Terakhir");
 }
+
+function validasi() {
+		var nama = document.getElementById("nama").value;
+		var password = document.getElementById("password").value;
+		if (nama != "" && password !="") {
+			alert('login berhasil');
+		}else{
+      alert('Anda harus mengisi data dengan lengkap !');
+    }
+}
+	
+var input = document.getElementById("password");
+var text = document.getElementById("text");
+input.addEventListener("keyup", function(event) {
+
+  if (event.getModifierState("CapsLock")) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none"
+  }
+})
+
+function pass(){
+ var pass = document.getElementById('password');
+ var show = document.getElementById('show');
+ if(pass.type == 'password'){
+  pass.type = 'text';
+  show.innerHTML = 'Sembunyikan';
+ }else{
+  pass.type = 'password';
+  show.innerHTML = 'Lihat Password'; 
+ }
+}
