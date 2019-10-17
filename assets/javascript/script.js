5c7913429c021eb356daeed52279d71106832d74
Dimas-Pratama-Purwadinata <54875915+Dimas-Pratama-Purwadinata@users.noreply.github.com>
2019-10-17 09:58:49 +0700

    Update script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,6 +14,17 @@ function validasi() {
       alert('Anda harus mengisi data dengan lengkap !');
     }
 }
+
+function validasiregistrasi(){
+    var nama = document.getElementById("nama").value;
+    var password = document.getElementById("password").value;
+    var email = document.getElementById("email").value;
+    if (nama != "" && password !="" && email !="") {
+      alert('login berhasil');
+    }else{
+      alert('Anda harus mengisi data dengan lengkap !');
+    }
+}
 	
 var input = document.getElementById("password");
 var text = document.getElementById("text");
@@ -25,6 +36,45 @@ input.addEventListener("keyup", function(event) {
     text.style.display = "none"
   }
 })
+
+
+function passwordStrength(p){
+ var status = document.getElementById('status');
+ var regex = new Array();
+ regex.push("[A-Z]");
+ regex.push("[a-z]");
+ regex.push("[0-9]");
+ regex.push("[!@#$%^&*]");
+
+ var passed = 0;
+  for(var x = 0; x < regex.length;x++){
+  if(new RegExp(regex[x]).test(p)){
+   console.log(passed++);
+  }
+ }
+
+	
+var strength = null;
+var color = null;
+switch(passed){
+  case 0:
+  case 1:
+  case 2:
+   strength = "Tidak Aman";
+   color = "#FF3232";
+  break;
+  case 3:
+   strength = "Cukup Aman";
+   color = "#E1D441";
+  break;
+  case 4:
+   strength = "Sangat Aman";
+   color = "#27D644";
+ }
+ status.innerHTML = strength;
+ status.style.color = color;
+}
+
 
 function pass(){
  var pass = document.getElementById('password');
