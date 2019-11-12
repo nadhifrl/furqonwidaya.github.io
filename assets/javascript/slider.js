e50985f6ec11f1c572c01589d0764606ec9367c6
furqonwidaya <FurqonWidaya@users.noreply.github.com>
2019-11-12 08:59:22 +0700

    update buat slider coy

---
+++ assets/javascript/slider.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    // Mengatur variabel
+    let speed = 100;            // kecepatan fade
+    let autoswitch = true;      // set automatis berganti
+    let autoswitch_speed = 2000 // kecepatan berganti
+
+    // menambahkan class slide pertama
+    $('.slide').first().addClass('active');
+
+    // menyembuyikan semua class slide
+    $('.slide').hide();
+
+    // menampilkan slide pertama
+    $('.active').show();
+
+    // intruksi auto slide
+    if(autoswitch == true){
+        setInterval(nextSlide,autoswitch_speed);
+    }
+
+    // berganti ke slide selanjutnya
+    function nextSlide(){
+        $('.active').removeClass('active').addClass('oldActive');
+        if($('.oldActive').is(':last-child')){
+            $('.slide').first().addClass('active');
+        } else {
+            $('.oldActive').next().addClass('active');
+        }
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+    }
+        });
+
