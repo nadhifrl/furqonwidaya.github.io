6f36d5d0b4e5f4ddc65f6d10d403cfdf333fcf87
furqonwidaya <FurqonWidaya@users.noreply.github.com>
2019-12-01 12:54:00 +0700

    Create preview_image.js

---
+++ assets/javascript/preview_image.js
@@ -0,0 +1,23 @@
+//fungsi untuk memunculkan save
+function save(){
+  alert("Saved Data")
+}
+function cancel() {
+            document.getElementById("preview").innerHTML = "";
+        }
+//fungsi untuk set mendapatkan data inputan setelah input gambar
+function preview(input) {
+   if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+          $('#anon').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+   }
+}
+
+
+//jquery untuk menampilkan gambar
+$("#preview").change(function(){
+   preview(this);
+});
